5dec388b4f4634bc94ce3429bc34260b04883719
AAGRcode <axel.gonzalez.280907@gmail.com>
2026-04-20 08:56:07 -0500

    Metodo Burbuja

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeros[] = {10, -5, 2, 0, 7};
+        runBubbleSort();
+        }
+
+    public static void runBubbleSort(){
+        System.out.println("Metodo Burbuja");
+        int numeros[] = {10, -5, 2, 0, 7};
+        //Instancia de la clase
+        BubbleSort bubbleSort = new BubbleSort();
+        // bubbleSort.printArreglo(numeros);
+        // bubbleSort.sortAscendente(numeros);
+        // bubbleSort.printArreglo(numeros);
+        // bubbleSort.sortDescendente(numeros);
+        // bubbleSort.printArreglo(numeros);
+            bubbleSort.printArreglo(numeros);            
+            bubbleSort.sort(numeros, true);
+            bubbleSort.printArreglo(numeros);
+            bubbleSort.sort(numeros, false);
+            bubbleSort.printArreglo(numeros);        
+
     }
 }
